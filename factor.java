130c84c7ae7ac7325d542b76096784aee1d8bc9f
Avinash Jha <86429000+akj2002276@users.noreply.github.com>
2025-09-12 20:11:22 +0530

    Update factor.java

--- factor.java
+++ factor.java
@@ -1,1 +1,15 @@
 
+
+class Main {
+    public static void main(String[] args) {
+        int n = 100; //1 2 5 10
+        System.out.print(1 + " ");
+        for(int i = 2; i<=n/2; i=i+1){
+            if(n % i == 0){
+                System.out.print(i + " ");
+            }
+        }
+        System.out.print(n);
+        
+    }
+}
